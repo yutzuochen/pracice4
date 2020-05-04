de2be21139852f16d8260edaf1044da150064b7b
yutzuochen <yutzuochen@gmail.com>
2020-05-04 16:53:51 +0800

    local change test

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Post
 # new test
+
+# local chage test
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 	list_display = ('title', 'slug', 'author', 'publish', 'status',)
